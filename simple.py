a416ad877a94e589a829ee01616b29694850ba24
Jyothika <krjyothika13@gmail.com>
2025-06-12 09:51:55 +0530

    Initial commit

---
+++ simple.py
@@ -0,0 +1,10 @@
+#a=10
+#b=20
+#a,b=b,a
+#print("a=",a,"b=", b)
+#print("a={} b={}".format(a,b))
+#print(f"a={a} b={b}")
+
+a="hgf"
+a=10
+print(type(a))
---
+++ withouttemp.py
@@ -0,0 +1,8 @@
+a=10
+b=20
+
+b=a+b
+a=b-a
+b=b-a
+print(a)
+print(b)
